a85b2ba3fda8965717fdc1ff11281a7641bdc760
Vladislav Nikitin <dushekrad@gmail.com>
2021-03-29 14:38:38 +0400

    feat(api): make room number an int

--- packages/api/src/rooms/types/create-room.input.ts
+++ packages/api/src/rooms/types/create-room.input.ts
@@ -1,7 +1,7 @@
-import { Field, InputType } from "@nestjs/graphql"
+import { Field, InputType, Int } from "@nestjs/graphql"
 
 @InputType({ description: "Input for creating a room" })
 export class CreateRoomInput {
-  @Field()
+  @Field((): typeof Int => Int, { description: "Number of a room to create" })
   number: number
 }
